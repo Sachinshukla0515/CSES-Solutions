06c7e85ca335dab3e7b0b7942f51b365fe58012c
SACHIN SHUKLA <64725812+Sachinshukla0515@users.noreply.github.com>
2021-08-25 22:34:21 +0530

    Create BitStrings.cpp

---
+++ BitStrings.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define ull unsigned long long
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i,a,b) for (int i=a;i<b;i++)
+typedef vector<int> VI;
+typedef pair<int,int>pi;
+int mod=1e9+7;
+ 
+ 
+ void solve(){
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    ll n;
+    cin>>n;
+   ll res=1;  
+   while(n--){
+     res<<=1;
+     res=res%mod;     
+   }
+   cout<<res<<"\n";
+ }
+ 
+int main() {
+    
+    solve();
+    return 0;
+}
