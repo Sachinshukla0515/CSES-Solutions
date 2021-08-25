26590229bafeb5a30b87d1b50050ac3c50f2a432
SACHIN SHUKLA <64725812+Sachinshukla0515@users.noreply.github.com>
2021-08-25 22:24:06 +0530

    Create WeirdAlgorithm.cpp

---
+++ WeirdAlgorithm.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(){
+     
+    ll n;
+    cin>>n;
+   
+    cout<<n<<" ";
+   
+    while(n!=1){
+      if(n%2==0){
+         n=n/2;
+         cout<<n<<" ";
+      }else{
+         n=n*3+1;
+         cout<<n<<" ";
+      }
+    }
+ 
+}
+ 
+ 
+int main() {
+    
+    solve();
+    return 0;
+}
+ 
